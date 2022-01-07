52df8c98e65c20deb0906ebef09928270c228018
Deshka <deshka_113@abv.bg>
2022-01-07 19:50:03 +0200

    style login and register page

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,32 @@
 import Chart from './Chart';
 import ProtectedRoute from './ProtectedRoute';
-export { Chart, ProtectedRoute };
+import { SFlexContainer } from './Form/Container/Container.styles';
+import {
+  SForm,
+  IconStyleWrapper,
+  StyledTitle,
+  StyledText,
+  StyledInput,
+  StyledError,
+  SmallStyledTitle,
+  StyledButton,
+  SRedirect,
+  SRedirectLabel,
+  SRedirectLink
+} from './Form/Form/Form.styles';
+export {
+  Chart,
+  ProtectedRoute,
+  SForm,
+  IconStyleWrapper,
+  StyledTitle,
+  StyledText,
+  StyledInput,
+  StyledError,
+  StyledButton,
+  SmallStyledTitle,
+  SFlexContainer,
+  SRedirect,
+  SRedirectLabel,
+  SRedirectLink
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 import Login from './Login';
 import NotFound from './NotFound';
-export { Login, NotFound };
+import Register from './Register';
+export { Login, NotFound,Register };
