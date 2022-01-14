e121dbf04c867ac1186e10764efc9e867d7bc02b
SashoT <sashotatarski@gmail.com>
2022-01-14 13:11:15 +0200

    Banner styled comp converted

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,58 +1,28 @@
-import { Container, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
+import {
+  StyledBannerContainer,
+  StyledBannerWrapper,
+  StyledTagline,
+  StyledTypographySubtitle,
+  StyledTypographyTitle
+} from './Banner.styles';
 import Carousel from './Carousel';
 
-const useStyles = makeStyles(() => ({
-  banner: {
-    backgroundImage: 'url(../banner2.jpg)'
-  },
-  bannerContent: {
-    height: 400,
-    display: 'flex',
-    flexDirection: 'column',
-    paddingTop: 25,
-    justifyContent: 'space-around'
-  },
-  tagline: {
-    display: 'flex',
-    height: '40%',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    textAlign: 'center'
-  }
-}));
-
 const Banner = () => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.banner}>
-      <Container className={classes.bannerContent}>
-        <div className={classes.tagline}>
-          <Typography
-            variant="h2"
-            style={{
-              fontWeight: 'bold',
-              marginBottom: 15,
-              fontFamily: 'Montserrat'
-            }}
-          >
+    <StyledBannerWrapper>
+      <StyledBannerContainer>
+        <StyledTagline>
+          <StyledTypographyTitle variant="h2">
             Crypto Dashboard
-          </Typography>
-          <Typography
-            variant="subtitle2"
-            style={{
-              color: 'darkgrey',
-              textTransform: 'capitalize',
-              fontFamily: 'Montserrat'
-            }}
-          >
+          </StyledTypographyTitle>
+          <StyledTypographySubtitle variant="subtitle2">
             Get all the Info regarding your favorite Crypto Currency
-          </Typography>
-        </div>
+          </StyledTypographySubtitle>
+        </StyledTagline>
         <Carousel />
-      </Container>
-    </div>
+      </StyledBannerContainer>
+    </StyledBannerWrapper>
   );
 };
 
---
+++ src/components/Banner/Banner.styles.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
+
+export const StyledBannerWrapper = styled.div`
+  background-image: url(../banner2.jpg);
+`;
+
+export const StyledBannerContainer = styled.div`
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  padding-top: 25px;
+  justify-content: space-around;
+`;
+export const StyledTagline = styled.div`
+  display: flex;
+  height: 40%;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+`;
+
+export const StyledTypographyTitle = styled(Typography)`
+ font-weight: bold;
+ margin-bottom: 15px;
+ font-family: 'Montserrat';
+`;
+
+export const StyledTypographySubtitle = styled(Typography)`
+  color: darkgrey;
+  text-transform: capitalize;
+  font-family: 'Montserrat';
+`;
